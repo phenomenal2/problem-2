5525d69a4b1f926535accc8170c2cba61157264d
phenomenal2 <66721355+phenomenal2@users.noreply.github.com>
2021-07-20 02:01:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class GFG
+{
+    static public void Main()
+    {
+
+        int[] price = { 2, 30, 15, 10, 8, 25, 80, 105 };
+
+
+        int profit = 0;
+
+
+        for (int i = 1; i < price.Length; i++)
+        {
+
+
+            int sub = price[i] - price[i - 1];
+            if (sub > 0)
+                profit += sub;
+        }
+        Console.WriteLine("Maximum Profit=" + profit);
+    }
+}
